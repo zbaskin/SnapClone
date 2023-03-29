3282acbfb75da7eb2db2b224f3b29ff262ec7403
Zach Baskin <zbaskin@gatech.edu>
2023-03-29 15:18:08 -0400

    create AbilityType enum

--- src/Ability.java
+++ src/Ability.java
@@ -1,3 +1,11 @@
 public class Ability {
     String abilityText;
+    enum AbilityType {
+        DESTROY,
+        ONGOING,
+        REVEAL,
+        DISCARD,
+        MOVE,
+        NONE
+    };
 }
