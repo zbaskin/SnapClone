ce9c94013d162c303a21edf3796815c89e550637
Zach Baskin <zbaskin@gatech.edu>
2023-04-04 13:15:44 -0400

    Impose max hand card count

--- src/Hand.java
+++ src/Hand.java
@@ -1,9 +1,11 @@
 public class Hand {
     Card[] hand;
     int handPower;
+    final int handCount;
 
     public Hand() {
         this.hand = null;
         this.handPower = 0;
+        this.handCount = 7;
     }
 }
