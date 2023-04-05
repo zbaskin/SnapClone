a1354f50c3d6c6aed831e142134afe6a4fb38f25
Zach Baskin <zbaskin@gatech.edu>
2023-04-05 15:17:55 -0400

    implement card cost

--- src/Card.java
+++ src/Card.java
@@ -1,9 +1,11 @@
 public class Card {
     int power;
+    int cardCost;
     Ability ability;
 
     public Card(Ability ability) {
         this.power = 0;
+        this.cardCost = 0;
         this.ability = ability;
     }
 }
