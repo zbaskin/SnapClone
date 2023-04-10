1fd808dd1c7f0fcbfa9af911fcf0c24e462557e7
Zach Baskin <zbaskin@gatech.edu>
2023-04-10 09:47:23 -0400

    create Location constructor

--- src/Location.java
+++ src/Location.java
@@ -3,4 +3,11 @@ public class Location {
     Ability locationAbility;
     Card[] playerHand;
     Card[] opponentHand;
+
+    public Location() {
+        this.locationPower = 0;
+        this.locationAbility = null;
+        this.playerHand = null;
+        this.opponentHand = null;
+    }
 }
