c4443d8c7323d4177089d084da373fae9d648c2c
Zach Baskin <zbaskin@gatech.edu>
2023-04-28 09:30:58 -0400

    new Card constructor

--- src/Card.java
+++ src/Card.java
@@ -3,6 +3,10 @@ public class Card {
     int cardCost;
     Ability ability;
 
+    public Card() {
+        this.power = 0;
+    }
+
     public Card(Ability ability) {
         this.power = 0;
         this.cardCost = 0;
